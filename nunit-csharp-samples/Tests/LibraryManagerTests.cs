decb8e8f7b2fa2e1ba79dc6fcf62913caf0a6a5f
Nasya Logunova <logunovanst2005@gmail.com>
2024-03-08 20:53:47 +0200

    Add Tests to LibraryManagerTests and LibraryStatisticsAnalyzerTests.

--- nunit-csharp-samples/Tests/LibraryManagerTests.cs
+++
@@ -1,6 +0,0 @@
-namespace nunit_csharp_samples.Tests;
-
-public class LibraryManagerTests
-{
-    // TODO step 4.
-}
--- nunit-csharp-samples/Tests/LibraryStatisticsAnalyzerTests.cs
+++
@@ -1,6 +0,0 @@
-namespace nunit_csharp_samples.Tests;
-
-public class LibraryStatisticsAnalyzerTests
-{
-    // TODO step 4.
-}
